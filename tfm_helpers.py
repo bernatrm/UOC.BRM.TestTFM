53453cd8c3c96d2d39f33bca22417e8bf6dd6469
Bernat Real <bernat.real@hotelsviva.com>
2022-01-15 19:47:27 +0100

    Generamos Reservas Cleaned

--- tfm_helpers.py
+++ tfm_helpers.py
@@ -16,15 +16,25 @@ def getSourcePathToHistoricalReservasDiariasHotel(path: str, hotel: str) -> str:
     """Given a root path and hotel, builds source stirng path to Historico Reservas Diarias"""
     return (fr"{path}\Historicos\ReservasDiarias\{hotel}")
 
-def getPathToCleanedHistoricalReservasHotel(path: str, hotel: str, format: str) -> str:
-    """Given a root path, hotel and format builds dest string path to Reservas"""
-    return (fr"{path}\historical\reservas\{format}\{hotel}_historico_reservas.{format}")
+def getPathToProcessedHistoricalReservasHotel(hotel: str, format: str) -> str:
+    """Given a hotel and format builds dest string path to Reservas"""
+    return (fr"data\processed\historical\reservas\{format}\{hotel}_historico_reservas.{format}")
 
-def getPathToCleanedHistoricalReservasDiariasHotel(path: str, hotel: str, format: str) -> str:
+def getPathToProcessedHistoricalReservasDiariasHotel(hotel: str, format: str) -> str:
+    """Given a hotel and format builds dest string path to Reservas Diarias"""
+    return (fr"data\processed\historical\reservas_diarias\{format}\{hotel}_historico_reservas_diarias.{format}")
+
+
+def getPathToCleanedHistoricalReservasHotel(format: str) -> str:
+    """Given a root path, hotel and format builds dest string path to Reservas"""
+    return (fr"data\cleaned\historical\reservas\{format}\historico_reservas.{format}")
+
+def getPathToCleanedHistoricalReservasDiariasHotel(format: str) -> str:
     """Given a root path, hotel and format builds dest string path to Reservas Diarias"""
-    return (fr"{path}\historical\reservas_diarias\{format}\{hotel}_historico_reservas_diarias.{format}")
+    return (fr"data\cleaned\historical\reservas_diarias\{format}\historico_reservas_diarias.{format}")
 
 to_integer = lambda i: (int(float(i)))
+hoteles = ["02", "04", "05", "15", "16", "21", "22"]
 
 if __name__ == "__main__":
     print("Helpers for TFM project")
